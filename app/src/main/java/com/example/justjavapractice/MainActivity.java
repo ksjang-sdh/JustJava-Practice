9c77a8ed35f29c8560e6c87c1ef35f59191f5227
unknown <ksjang@sdh.hs.kr>
2020-06-15 12:54:34 +0900

    iiëë¯¸ê°ì¼ë¡ íë©´ì ëíë´ê¸° ì¶

--- app/src/main/java/com/example/justjavapractice/MainActivity.java
+++ app/src/main/java/com/example/justjavapractice/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.justjavapractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        displayQuantity(3);
+        displayPrice(3);
+    }
+
+    private void displayPrice(int number) {
+        int coffeePrice = 5000;
+        TextView priceTextView = (TextView) findViewById(R.id.text_price);
+        priceTextView.setText(NumberFormat
+                .getCurrencyInstance(Locale.KOREA).format(number*coffeePrice));
+    }
+
+    private void displayQuantity(int number) {
+        TextView quantityTextView = findViewById(R.id.text_quantity);
+        quantityTextView.setText("" + number); // "0"
     }
 }
